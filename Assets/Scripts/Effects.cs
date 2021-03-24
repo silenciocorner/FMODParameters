ad97e19b0f023a28df61b0744b3b4e5c8609b9cc
momnus <momnus@ya.ru>
2021-03-24 14:33:03 +0300

    load

---
+++ Assets/Scripts/Effects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+    [SerializeField] [Range(0f, 1f)]
+    private float reverb, delay;
+
+    [SerializeField] [Range(0f, 5000f)]
+    private float delayTime;
+
+    [SerializeField] [Range(-12f, 12f)]
+    private float pitch;
+
+
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+    void Update()
+    {
+        instance.setParameterByName("Reverb", reverb);
+        instance.setParameterByName("Delay", delay);
+        instance.setParameterByName("DelayTime", delayTime);
+        instance.setParameterByName("Pitch", pitch);
+    }
+}
---
+++ Assets/Scripts/GlobalParameter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalParameter : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float eq;
+
+    void Update()
+    {
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("EQ Global", eq);
+    }
+}
